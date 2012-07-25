eaacad3da9ab353838d59b8445ff2e5213fd922d
ganlv <zhoutao198712@163.com>
2012-07-25 10:56:36 +0800

    Initial Commit

---
+++ LoadMore/GLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GLAppDelegate.h
+//  LoadMore
+//
+//  Created by IOS开发 on 12-7-25.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LoadMore/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LoadMore
+//
+//  Created by IOS开发 on 12-7-25.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GLAppDelegate class]));
+    }
+}
